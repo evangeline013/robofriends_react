1d61bb83b4032fbff6fdc8b42978d280d76e9cf3
Eva <evangeline013@gmail.com>
2018-12-07 16:46:14 -0500

    Add CardList component

---
+++ src/CardList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from './Card';
+
+
+class CardList extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.robots.map(user => {
+            return <Card key={user.id} id={user.id} name={user.name} email={user.email} />
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default CardList;
