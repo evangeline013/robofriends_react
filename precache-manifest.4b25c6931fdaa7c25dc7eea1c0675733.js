c03f40f88377c0051d9ce3f6207872ac36a839c5
Eva <evangeline013@gmail.com>
2018-12-10 16:58:19 -0500

    Updates

---
+++ precache-manifest.4b25c6931fdaa7c25dc7eea1c0675733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b3f6d47e65f2da9b52f",
+    "url": "/robofriends_react/static/js/runtime~main.9b3f6d47.js"
+  },
+  {
+    "revision": "3416f37f4a3d9b5f6b8a",
+    "url": "/robofriends_react/static/js/main.3416f37f.chunk.js"
+  },
+  {
+    "revision": "8fcca33aadc16a7113bd",
+    "url": "/robofriends_react/static/js/1.8fcca33a.chunk.js"
+  },
+  {
+    "revision": "3416f37f4a3d9b5f6b8a",
+    "url": "/robofriends_react/static/css/main.a54032b8.chunk.css"
+  },
+  {
+    "revision": "8fcca33aadc16a7113bd",
+    "url": "/robofriends_react/static/css/1.52096db2.chunk.css"
+  },
+  {
+    "revision": "678510c34f24c406d8a8fb123451dd34",
+    "url": "/robofriends_react/index.html"
+  }
+];
